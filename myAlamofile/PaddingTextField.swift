83c202912088425ff7adac5a6dbda603324d1e7d
Nutchatchai <chatchai.in@ku.th>
2020-07-10 13:39:33 +0700

    first commit

---
+++ myAlamofile/PaddingTextField.swift
@@ -0,0 +1,21 @@
+//
+//  PaddingTextField.swift
+//  myAlamofile
+//
+//  Created by Nutchatchai on 9/7/2563 BE.
+//  Copyright © 2563 Nutchatchai. All rights reserved.
+//
+
+import UIKit
+
+class PaddingTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ myAlamofile/WeatherResponse.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherResponse.swift
+//  myAlamofile
+//
+//  Created by Nutchatchai on 1/7/2563 BE.
+//  Copyright © 2563 Nutchatchai. All rights reserved.
+//
+
+import Foundation
